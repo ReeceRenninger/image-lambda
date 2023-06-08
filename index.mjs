21a5ace188742ad6e1154ef1fe287466fb68cc66
ReeceRenninger <renningerreece@gmail.com>
2023-06-08 10:31:41 -0700

    its working DO NOT TOUCH PLEASE

--- index.mjs
+++ index.mjs
@@ -2,60 +2,62 @@
 import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Response } from 'node-fetch';
 
-const s3Client = new S3Client({region: 'us-west-1'});
 
 export const handler = async (event) => {
   console.log('this is my event', event);
-  
-  const Bucket = 'reece-images';
-  const Key = 'images.json';
-  console.log({ Bucket, Key });
-  
+
+  let s3Client = new S3Client({ region: 'us-west-1' });
   //TODO implement
-  let fileName = event.Records[0].s3.object.key;
+  let name = event.Records[0].s3.object.key;
   let size = event.Records[0].s3.object.size;
   let type = 'jpg';
-  let newImageObject = { fileName, size, type };
-  console.log('this is my new imageobject', newImageObject);
+  let newImageObject = { name, size, type };
+  console.log('this is my new imageobject', newImageObject); // proof of life for uploading an image to bucket
 
-  
+  // getobjectcommand requires these two things to be fed to it, the Bucket and Key
   let params = {
-    Bucket,
-    Key,
+    Bucket: 'reece-images',
+    Key: 'images.json',
   };
-  console.log('THESE ARE MY PARAMS', params);
-  
-  let data;
-  // grab all objects in the bucket, I am able to grab the images.json but I have to hardcode images.json INTO THE KEY PROPERTY ON THE ACTUAL TEST
-  try {
-    let s3results = await s3Client.send(new GetObjectCommand(params));
-    const response = new Response (s3results.Body);
-    data = await response.json();
-    data.push(newImageObject);
-    params.Body = JSON.stringify(data);
-    console.log('this is my data after my push to data request', data);
-  } catch (error) {
-    console.warn('error: ', error);
-  }
-  
 
-  // modify or create a new object in the bucket
+  console.log('THESE ARE MY PARAMS', params);
+
+  let imageData;
+  // grab all objects in the bucket
+  try {
+    let results = await s3Client.send(new GetObjectCommand(params));
+    let response = new Response(results.Body);
+    let retrievedImageData = await response.json(); // converts response into a useable array 
+    imageData = retrievedImageData; //we assign the useable array to our established variable
+
+    console.log('this is my imageData after GET request', imageData);
+  } catch (error) {
+    console.warn('GET object error: ', error);
+    imageData = [];
+  }
+  imageData.push(newImageObject);
+  console.log('my image in my imageData array', imageData);
+
+
+  let stringifiedDetails = JSON.stringify(imageData);
+  // modify or create a new object in the bucket, this does creation of a new images.json if none are present in bucket
+  console.log('this is my stringifieddeatils', stringifiedDetails);
   let newParams = {
     ...params,
-    Key: 'images.json',
-    ContentType: 'application/json',
-  };
+    Body: stringifiedDetails,
+    ContentType: 'application/json', //for Json, its always this
+  }
 
   try {
     await s3Client.send(new PutObjectCommand(newParams));
   } catch (error) {
-    console.log('error: ', error);
-    
+    console.warn('Failed to PUT error: ', error);
+
   }
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(newParams),
+    body: stringifiedDetails, // this may need to change
   };
   return response;
 };
